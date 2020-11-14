96f190b721227fecbb43a3945b6bd514c1b90687
sky2145 <74121357+sky2145@users.noreply.github.com>
2020-11-14 16:15:29 +0900

    Initial commit

---
+++ Assets/cloud contoroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudcontoroller : MonoBehaviour
+{
+    //最小サイズ
+    private float minimum = 1.0f;
+    //拡大縮小スピード
+    private float magSpeed = 10.0f;
+    //拡大率
+    private float magnification = 0.07f;
+
+  
+    }
+    // use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    //雲を拡大縮小
+    this.transform.localscale = new Vector3(this.minimum + Mathf.Sin(Time.time*this.magSpeed)*this.magnification,transform.localScale.y,this.minimum+Mathf.Sin(Time.time*this.magSpeed)*this.magnification)
+        
+    }
+}
