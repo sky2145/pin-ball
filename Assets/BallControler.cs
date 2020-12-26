a041dea94399c39e7b5379c507d4b8834d187e2f
sky2145 <74121357+sky2145@users.noreply.github.com>
2020-12-26 18:02:13 +0900

    変更

---
+++ Assets/BallControler.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallControler : MonoBehaviour
+{
+    //得点を表示するテキスト
+    private GameObject ScoreText1;
+​
+    //得点を格納する変数
+    int score = 0;
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject GameoverText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.GameoverText = GameObject.Find("GameOverText");
+        ScoreText1 = GameObject.Find("スコアを表示するためのTextコンポーネントがアタッチされたオブジェクト名をここに指定する");
+    }
+    
+
+
+// Update is called once per frame
+void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.GameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+            Debug.Log("現在の点数は " + score.ToString() + " 点です。TODO： 点数を画面に表示する");
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+            Debug.Log("現在の点数は " + score.ToString() + " 点です。TODO： 点数を画面に表示する");
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            score += 30;
+            Debug.Log("現在の点数は " + score.ToString() + " 点です。TODO： 点数を画面に表示する");
+        }
+    }
+
+}
